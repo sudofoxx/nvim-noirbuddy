9762a00ac8d7c6671be578e490495d405a9e9bb2
sudo fox <kucharek.marvin@gmail.com>
2025-09-08 23:39:33 +0200

    Added light, dark, and dark+ color variations of vscode monochromator theme

---
+++ lua/noirbuddy/presets/dark-ruby.lua
@@ -0,0 +1,24 @@
+-- Inspired by Josias' Beem Monochromator theme for VS Code
+
+return {
+  background = '#1D1D20', -- Original #121212
+  primary = '#ed333b',
+  secondary = '#d5d5d5',
+  noir_0 = '#ffffff',
+  noir_1 = '#f5f5f5',
+  noir_2 = '#d5d5d5',
+  noir_3 = '#b4b4b4',
+  noir_4 = '#a7a7a7',
+  noir_5 = '#949494',
+  noir_6 = '#737373',
+  noir_7 = '#535353',
+  noir_8 = '#323232',
+  noir_9 = '#212121',
+  diagnostic_error = '#ff0038',
+  diagnostic_warning = '#ff7700',
+  diagnostic_info = '#47eae0',
+  diagnostic_hint = '#47eae0',
+  diff_add = '#00ff77',
+  diff_change = '#47eae0',
+  diff_delete = '#ff0038',
+}
